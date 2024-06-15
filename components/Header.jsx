801dce68401bc6b9dbf33336b604db01920fd81a
Jonathan Vinicius <jonathantotini@gmail.com>
2024-06-15 11:40:41 -0500

    Sua mensagem de commit

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = ({ title }) => {
+  return (
+    <div className="flex justify-between p-4">
+      <h1 className="font-bold text-8">{title}</h1>
+      <h2>
+        Welcome back, <strong>Jonathan</strong>{" "}
+      </h2>
+    </div>
+  );
+};
+
+export default Header;
